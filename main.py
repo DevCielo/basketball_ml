7547bbd81b826a15af7b33d088c5d38ea910d61f
DevCielo <136787904+DevCielo@users.noreply.github.com>
2025-05-05 13:29:10 +1000

    setup a player tracker with the opencv

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from utils import read_video, save_video
+from trackers import PlayerTracker
 
 def main():
 
     # Read Video
     video_frames = read_video("input_videos/video_1.mp4")
+
+    # Initialize Tracker
+    player_tracker = PlayerTracker("models/player_detector.pt")
+
+    # Run Trackers
+    player_tracks = player_tracker.get_object_tracks(video_frames)
+
+    print(player_tracks)
 
     # Save Video
     save_video(video_frames, "output_videos/output_video.avi")
