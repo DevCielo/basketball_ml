0b32f39e766193c1bdafc8872f75016db3633081
DevCielo <136787904+DevCielo@users.noreply.github.com>
2025-05-07 13:40:37 +1000

    setup the ball tracker drawing for the basketball tracking with an arrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from utils import read_video, save_video
-from trackers import PlayerTracker
-from drawers import PlayerTracksDrawer
+from trackers import PlayerTracker, BallTracker
+from drawers import PlayerTracksDrawer, BallTracksDrawer
 
 def main():
 
@@ -9,16 +9,21 @@ def main():
 
     # Initialize Tracker
     player_tracker = PlayerTracker("models/player_detector.pt")
+    ball_tracker = BallTracker("models/ball_detector_model.pt")
 
     # Run Trackers
     player_tracks = player_tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path="stubs/player_track_stubs.pkl")
+    ball_tracks = ball_tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path="stubs/ball_track_stubs.pkl")
 
     # Draw Output
     # Initialize Drawer
     player_tracks_drawer = PlayerTracksDrawer()
+    ball_tracks_drawer = BallTracksDrawer()
+
 
     # Draw Object Tracks
     output_video_frames = player_tracks_drawer.draw(video_frames, player_tracks)
+    output_video_frames = ball_tracks_drawer.draw(output_video_frames, ball_tracks)
 
     # Save Video
     save_video(output_video_frames, "output_videos/output_video.avi")
